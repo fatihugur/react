3edc2358948b17e2c9e0f2848f6bbf2d717401b5
fatihugur@gmail.com <fatihugur@gmail.com>
2020-09-04 15:02:49 +0200

    react

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>İlk React sayfama hoşgeldiniz</h1>
       </header>
+      <section>
+        <p>Lorem ipsum tekst</p>
+      </section>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +15,31 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const myfirstelement = <h1>Welcome my first react page!</h1>
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>Fatih</td>
+    </tr>
+    <tr>
+      <td>Yavuz</td>
+    </tr>
+  </table>
+);
+const mytitle = <h3>Everybody kan learn React easly</h3>
+const myopinion = <h3>Because React is {5 + 5} times beter with JSX and other syntax</h3>
+
+ReactDOM.render(<p>Hallo I am Fatih</p>, document.getElementById('sandy'));
+ReactDOM.render(myelement, document.getElementById('root2'));
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(mytitle, document.getElementById('title'));
+ReactDOM.render(myopinion, document.getElementById("opinion"));
+ReactDOM.render(<p>Copyright, 2020</p>, document.getElementById("copyright"));
